ef7f6fd479a52985ebe048c51a9a72781133372b
Vishal Chauhan <148201865+vishalchauhan2109@users.noreply.github.com>
2025-07-04 23:41:10 +0530

    Node to Root Path Generic tree

---
+++ code (14).java
@@ -0,0 +1,61 @@
+import java.util.*;
+
+public class Main {
+
+    public static class Node {
+        int data;
+        ArrayList<Node> children = new ArrayList<>();
+    }
+    public static ArrayList<Integer> PathToRoot(Node node ,int val){
+        if(node.data == val){
+            ArrayList<Integer> list = new ArrayList<>();
+            list.add(node.data);
+            return list;
+        }
+        
+        for(Node child: node.children){
+            ArrayList<Integer> list = PathToRoot(child,val);
+            
+            if(list.size() > 0){
+                list.add(node.data);
+                return list;
+            }
+        }
+        return new ArrayList<>();
+    }
+
+ 
+    
+    
+
+    public static void main(String[] args) {
+        int[] arr = {10, 20, 50, -1, 60, -1, -1,
+                     30, 70, -1, 80, 110, -1, 120, -1, -1,
+                     90, -1, -1, 40, 100, -1, -1, -1};
+
+        Stack<Node> st = new Stack<>();
+        Node root = null;
+
+        for(int val : arr){
+            if(val == -1){
+                st.pop();
+            } else {
+                Node t = new Node();
+                t.data = val;
+
+                if(!st.isEmpty()){
+                    st.peek().children.add(t);
+                } else {
+                    root = t;
+                }
+
+                st.push(t);
+            }
+        }
+
+        // display(root);
+        // oneline(root);
+        // display(root);
+        System.out.println(PathToRoot(root,100));
+    }
+}
